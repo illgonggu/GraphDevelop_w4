e41611207a4df6c50914b455664df9756a48fd4a
illgonggu <rbfl2365@icloud.com>
2022-12-04 18:05:59 +0900

    Initial Commit

---
+++ GraphDevelop_w4/GraphDevelop_w4App.swift
@@ -0,0 +1,17 @@
+//
+//  GraphDevelop_w4App.swift
+//  GraphDevelop_w4
+//
+//  Created by Loopy on 2022/12/04.
+//
+
+import SwiftUI
+
+@main
+struct GraphDevelop_w4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
